087d89b0bfe1e7d20ae5228affdcc173ce1932f2
Harald Kirschner <hkirschner@mozilla.com>
2015-07-09 14:06:05 -0700

    Working on 2.0-3.0.

--- index.js
+++ index.js
@@ -1,94 +1,110 @@
 (function app() {
-	'use strict';
+  'use strict';
 
-	var eventName = document.ontouchdown ? 'touchdown' : 'mousedown';
+  var eventName = document.ontouchdown ? 'touchdown' : 'mousedown';
 
-	function setupCamera(next) {
-		if (!navigator.mozCameras) {
-			return next('No camera');
-		}
+  function setupCamera(next) {
+    if (!navigator.mozCameras) {
+      return next('No camera');
+    }
 
-		function found(camera) {
-			count--;
-			if (camera) {
-				var flashModes = camera.capabilities.flashModes;
-				console.log('flashModes: ' + flashModes);
-				if (flashModes && flashModes.indexOf('torch') >= 0) {
-					console.log('Found my torch!');
-					count = -1;
-					next(null, camera);
-				}
-			}
-			if (!count) {
-				next('No flash');
-			}
-		}
+    function found(camera) {
+      if (count <= 0) {
+        return;
+      }
+      count--;
+      if (camera) {
+        var flashModes = camera.capabilities.flashModes;
+        console.log('flashModes: ' + flashModes);
+        if (flashModes && flashModes.indexOf('torch') >= 0) {
+          count = -1;
+          next(null, camera);
+        }
+      }
+      if (!count) {
+        next('No flash');
+      }
+    }
 
-		var cameras = navigator.mozCameras.getListOfCameras();
-		var count = cameras.length;
-		console.log('cameras: ' + count);
-		for (var i = 0; i < cameras.length; i++) {
-			var promise = navigator.mozCameras.getCamera({
-				camera: cameras[i]
-			}, found);
-			if (promise instanceof Promise) {
-				promise.then(function(result) {
-					console.log('promise', result);
-					found(result.camera);
-				});
-			}
-		}
-		if (!count) {
-			next('No camera');
-		}
-	}
+    function getCameraPromise(id) {
+      var promise = navigator.mozCameras.getCamera({
+        camera: cameras[i]
+      }, {mode: 'unspecified'}).then(function(result) {
+        found(result.camera);
+      });
+    }
 
-	var torched = false;
-	var currentCamera = null;
+    function getCameraCallback(id) {
+      navigator.mozCameras.getCamera({
+        camera: id
+      }, null, found, function(err) {
+        throw new Error(err);
+      });
+    }
 
-	function trigger(to, release) {
-		torched = (to != null) ? to : (!torched);
+    var oldStyle = navigator.mozCameras.getCamera.length == 3;
+    console.log('oldStyle', oldStyle);
+    var getCamera = oldStyle ? getCameraCallback : getCameraPromise;
 
-		if (torched) {
-			document.body.classList.add('torching');
-		} else {
-			document.body.classList.remove('torching');
-		}
+    var cameras = navigator.mozCameras.getListOfCameras();
+    var count = cameras.length;
+    console.log('getListOfCameras: ' + count);
+    for (var i = 0; i < count; i++) {
+      console.log('getCamera:', cameras[i]);
+      getCamera(cameras[i]);
+    }
+    if (!count) {
+      next('No camera');
+    }
+  }
 
-		if (currentCamera) {
-			currentCamera.flashMode = (torched) ? 'torch' : 'auto';
-			return;
-		}
+  var torched = false;
+  var currentCamera = null;
 
-		console.log('Calling setupCamera');
-		setupCamera(function(err, camera) {
-			if (!camera) {
-				console.warn(err);
-				document.body.classList.add('unsupported');
-				return;
-			}
-			document.body.classList.add('supported');
-			currentCamera = camera;
-			console.log('Setting flashMode');
-			camera.flashMode = (torched) ? 'torch' : 'auto';
-		});
-	}
+  function trigger(to, release) {
+    torched = (to != null) ? to : (!torched);
 
-	document.body.addEventListener(eventName, function(evt) {
-		evt.preventDefault();
-		trigger();
-	});
+    if (torched) {
+      document.body.classList.add('torching');
+    } else {
+      document.body.classList.remove('torching');
+    }
 
-	document.addEventListener('visibilitychange', function() {
-		if (document.hidden) {
-			trigger(false);
-			if (currentCamera) {
-				currentCamera.release(function() {
-					console.log('Camera released');
-				});
-				currentCamera = null;
-			}
-		}
-	}, false);
+    if (currentCamera) {
+      currentCamera.flashMode = (torched) ? 'torch' : 'auto';
+      console.log('Set flashMode:', currentCamera.flashMode);
+      return;
+    }
+
+    console.log('Calling setupCamera');
+    setupCamera(function(err, camera) {
+      if (!camera) {
+        console.warn(err);
+        document.body.classList.add('unsupported');
+        return;
+      }
+      document.body.classList.add('supported');
+      currentCamera = camera;
+      console.log('Setting flashMode');
+      camera.flashMode = (torched) ? 'torch' : 'auto';
+    });
+  }
+
+  document.body.addEventListener(eventName, function(evt) {
+    evt.preventDefault();
+    trigger();
+  });
+
+  document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+      trigger(false);
+      if (currentCamera) {
+        currentCamera.release(function() {
+          console.log('Camera released');
+        });
+        currentCamera = null;
+      }
+    }
+  }, false);
 
 })();
